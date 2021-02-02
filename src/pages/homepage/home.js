968f99cf3e02a5ceafc29ea9ae01c0f059ad91ac
idahdam <nasshitell@gmail.com>
2021-02-02 19:52:41 +0700

    first commit

---
+++ src/pages/homepage/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Home = () => {
+    return (
+        <>
+        	<div>
+        		Hello! This is a documentation for bot diprat. I'm making this by myself on my spare time so relax. 
+        	</div>
+        </>
+    )
+}
+
+export default Home;
